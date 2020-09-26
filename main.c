b7698f4b363a3ac27e479f70bb4776d4f1aa28a7
AndreiNekrasOn <an-32010@yandex.ru>
2020-09-26 16:18:29 +0300

    Formatting code, clang-format config

--- main.c
+++ main.c
@@ -2,61 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum {
+enum
+{
     DEFAULT_STRING_CAP = 32
 };
 
-typedef struct list {
-    char *word;
-    struct list *next;
+typedef struct list
+{
+    char* word;
+    struct list* next;
 } list;
 
-typedef struct sized_string {
-    char *str;
-    int size;
-    int capacity;
-} sized_string;
-
-char *reallocate_str(char *str, int size, int *capacity) {
-    if (size + 1 == *capacity) {
+char* reallocate_str(char* str, int size, int* capacity)
+{
+    if (size + 1 == *capacity)
+    {
         *capacity *= 2;
         str = realloc(str, *capacity);
     }
     return str;
 }
 
-sized_string *update_str_cap(sized_string *sstr) {
-    if (sstr->size + 1 == sstr->capacity) {
-        sstr->capacity *= 2;
-        sstr->str = realloc(sstr->str, sstr->capacity);
-    }
-    return sstr;
-}
-
-list *list_init() {
-    list *l = malloc(sizeof(*l));
+list* list_init()
+{
+    list* l = malloc(sizeof(*l));
     l->next = NULL;
     l->word = NULL;
     return l;
 }
 
-void list_print(const list *head) {
-    if (head == NULL) return;
-    if (head->word == NULL) exit(69); /* debug */
+void list_print(const list* head)
+{
+    if (head == NULL)
+        return;
     printf("%s\n", head->word);
     list_print(head->next);
 }
 
-void list_free(list *head) {
-    if (head == NULL) return;
-    list *next = head->next;
+void list_free(list* head)
+{
+    if (head == NULL)
+        return;
+    list* next = head->next;
     free(head->word);
     free(head);
     list_free(next);
 }
 
-char *update_str(char *str, char symbol, int *size, int *capacity) {
-    if (str == NULL) { /* word is not initiated */
+char* update_str(char* str, char symbol, int* size, int* capacity)
+{
+    if (str == NULL) /* word is not initiated */
+    {
         str = malloc(*capacity);
         *size = 0;
     }
@@ -66,62 +62,77 @@ char *update_str(char *str, char symbol, int *size, int *capacity) {
     return str;
 }
 
-list *list_insert(list *tail) {
-    if (tail == NULL) {
+list* list_insert(list* tail)
+{
+    if (tail == NULL)
         return list_init();
-    }
-    list *child = list_init();
+    list* child = list_init();
     tail->next = child;
     return child;
 }
 
-char *scan_command() {
+char* scan_command()
+{
     int symbol;
     int size = 0;
     int capacity = DEFAULT_STRING_CAP;
-    char *command_str = NULL;
-    while ((symbol = getchar()) != EOF) {
-        if (symbol == '\n') {
-            return command_str; 
-        } 
-        command_str = update_str(command_str, symbol, &size, &capacity); 
+    char* command_str = NULL;
+    while ((symbol = getchar()) != EOF)
+    {
+        if (symbol == '\n')
+            return command_str;
+        command_str = update_str(command_str, symbol, &size, &capacity);
     }
     return command_str;
 }
 
-list *parse_command(const char *command) {
-    if (command == NULL) {
-        return NULL;
+int start_new_word(int new_word_flag, list* tail)
+{
+    if (new_word_flag)
+    {
+        tail = list_insert(tail);
+        new_word_flag = 0;
     }
-    list *head = NULL; 
-    list *tail = NULL;
-    int word_size = 0;
-    int word_cap = DEFAULT_STRING_CAP;
-    int quote_flag = 0;
-    int new_word_flag = 1;
+    return new_word_flag;
+}
+
+void mutate_to_default(int* word_size, int* word_cap, int* quote_flag, int* new_word_flag)
+{
+    *word_size = 0;
+    *word_cap = DEFAULT_STRING_CAP;
+    *quote_flag = 0;
+    *new_word_flag = 1;
+}
+
+list* parse_command(const char* command)
+{
+    if (command == NULL)
+        return NULL;
+    list* head = NULL;
+    list* tail = NULL;
+    int word_size, word_cap, quote_flag, new_word_flag;
+    mutate_to_default(&word_size, &word_cap, &quote_flag, &new_word_flag);
     int i;
-    for (i = 0; command[i] != '\0'; i++) {
-        if (command[i] == ' ' && !quote_flag) {
-            if (word_size == 0) {
+    for (i = 0; command[i] != '\0'; i++)
+    {
+        if (command[i] == ' ' && !quote_flag)
+        {
+            if (word_size == 0)
                 continue;
-            }
-            new_word_flag = 1;
-            quote_flag = 0;
-            word_size = 0;
-            word_cap = DEFAULT_STRING_CAP;
-        } else {
-            if (command[i] == '"') quote_flag = !quote_flag;
-            if (new_word_flag) {
-                tail = list_insert(tail);
-                new_word_flag = 0;
-            }
-            if (head == NULL) {
+            mutate_to_default(&word_size, &word_cap, &quote_flag, &new_word_flag);
+        }
+        else
+        {
+            if (command[i] == '"')
+                quote_flag = !quote_flag;
+            new_word_flag = start_new_word(new_word_flag, tail);
+            if (head == NULL)
                 head = tail;
-            }
             tail->word = update_str(tail->word, command[i], &word_size, &word_cap);
         }
     }
-    if (quote_flag) {
+    if (quote_flag)
+    {
         fprintf(stderr, "Error - unbalanced quotes\n");
         list_free(head);
         return NULL;
@@ -129,13 +140,14 @@ list *parse_command(const char *command) {
     return head;
 }
 
-int main() {
-    while (!feof(stdin)) {
+int main()
+{
+    while (!feof(stdin))
+    {
         printf(">>");
-        list *command = parse_command(scan_command());
+        list* command = parse_command(scan_command());
         list_print(command);
         list_free(command);
-    } 
-    puts("\n----------------");
+    }
     return 0;
 }
